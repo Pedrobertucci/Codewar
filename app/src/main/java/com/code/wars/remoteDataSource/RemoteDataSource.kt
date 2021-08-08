174f7969043b6eb28f484a1477433283dc9e2e69
pedro <bertucci.dev@gmail.com>
2021-08-08 14:15:02 +0100

    was add api contract for all calls

---
+++ app/src/main/java/com/code/wars/remoteDataSource/RemoteDataSource.kt
@@ -0,0 +1,31 @@
+package com.code.wars.remoteDataSource
+
+import com.code.wars.models.AuthoredResponse
+import com.code.wars.models.CompletedResponse
+import com.code.wars.models.UserResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+interface RemoteDataSource {
+
+    @Headers("Content-Type: application/json")
+    @GET("api/v1/users/{user}")
+    suspend fun getUser(
+        @Path("user") user: String
+    ) : Response<UserResponse>
+
+    @Headers("Content-Type: application/json")
+    @GET("api/v1/users/{user}/code-challenges/completed?page={page}")
+    suspend fun getCompletedCodeChallenges(
+        @Path("user") user: String,
+        @Path("page") page: Int,
+    ) : Response<CompletedResponse>
+
+    @Headers("Content-Type: application/json")
+    @GET("api/v1/users/{user}/code-challenges/authored")
+    suspend fun getAuthoredChallenges(
+        @Path("user") user: String,
+    ) : Response<AuthoredResponse>
+}
