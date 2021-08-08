6916011c43d4e1592b6c3e95940fdc5e6f8d133e
pedro <bertucci.dev@gmail.com>
2021-08-08 19:31:01 +0100

    was add userViewModel for received data on repository

---
+++ app/src/main/java/com/code/wars/viewModels/UserViewModel.kt
@@ -0,0 +1,108 @@
+package com.code.wars.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.code.wars.models.AuthoredResponse
+import com.code.wars.models.CompletedResponse
+import com.code.wars.models.UserResponse
+import com.code.wars.remoteDataSource.SafeResponse
+import com.code.wars.repositories.UserRepository
+import kotlinx.coroutines.launch
+import org.jetbrains.annotations.NotNull
+import javax.inject.Inject
+
+class UserViewModel @Inject constructor(
+    private val repository: UserRepository) : ViewModel() {
+
+    private val userMutableData = MutableLiveData<UserResponse>()
+    val userLiveData: LiveData<UserResponse> get() = userMutableData
+
+    private val completedMutableData = MutableLiveData<CompletedResponse>()
+    val completedLiveData: LiveData<CompletedResponse> get() = completedMutableData
+
+    private val authoredMutableData = MutableLiveData<AuthoredResponse>()
+    val authoredLiveData: LiveData<AuthoredResponse> get() = authoredMutableData
+
+    private val emptyValuesMutableData = MutableLiveData<Boolean>()
+    val emptyValuesLiveData: LiveData<Boolean> get() = emptyValuesMutableData
+
+    private val errorMutableData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String> get() = errorMutableData
+
+    fun getUser(@NotNull user: String) {
+        viewModelScope.launch {
+            when(val response = repository.getUser(user)) {
+                is SafeResponse.NetworkError -> {
+                    errorMutableData.value = "Network Error"
+                }
+
+                is SafeResponse.GenericError -> {
+                    errorMutableData.value = response.error
+                }
+
+                is SafeResponse.Success -> {
+                    response.value.body()?.let {
+                        if (it.username.isNotEmpty())
+                            userMutableData.value = it
+                        else
+                            emptyValuesMutableData.value = true
+                    } ?: run {
+                        emptyValuesMutableData.value = true
+                    }
+                }
+            }
+        }
+    }
+
+    fun getCompletedCodeChallenge(@NotNull user: String) {
+        viewModelScope.launch {
+            when(val response = repository.getCompletedCodeChallenge(user)) {
+                is SafeResponse.NetworkError -> {
+                    errorMutableData.value = "Network Error"
+                }
+
+                is SafeResponse.GenericError -> {
+                    errorMutableData.value = response.error
+                }
+
+                is SafeResponse.Success -> {
+                    response.value.body()?.let {
+                        if (it.completed.isEmpty())
+                            emptyValuesMutableData.value = true
+                        else
+                            completedMutableData.value = it
+                    } ?: run {
+                        emptyValuesMutableData.value = true
+                    }
+                }
+            }
+        }
+    }
+
+    fun getAuthoredChallenges(@NotNull user: String) {
+        viewModelScope.launch {
+            when(val response = repository.getAuthoredChallenges(user)) {
+                is SafeResponse.NetworkError -> {
+                    errorMutableData.value = "Network Error"
+                }
+
+                is SafeResponse.GenericError -> {
+                    errorMutableData.value = response.error
+                }
+
+                is SafeResponse.Success -> {
+                    response.value.body()?.let {
+                        if (it.challenges.isEmpty())
+                            emptyValuesMutableData.value = true
+                        else
+                            authoredMutableData.value = it
+                    } ?: run {
+                        emptyValuesMutableData.value = true
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/code/wars/viewModels/UserViewModelTest.kt
@@ -0,0 +1,194 @@
+package com.code.wars.viewModels
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.code.wars.repositories.FakeUserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+class UserViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: UserViewModel
+
+    private val fakeRepository = FakeUserRepository()
+
+    @Mock
+    private var context: Context = mock(Context::class.java)
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(context)
+
+        viewModel = UserViewModel(repository = fakeRepository)
+    }
+
+    @Test
+    fun `should fetch user and return a generic error`() = runBlockingTest {
+        networkError()
+
+        viewModel.getUser("john123")
+
+        val response = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(response.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user and return http error`() = runBlocking {
+        httpError()
+
+        viewModel.getUser("john123")
+
+        val response = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(response.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user and return empty`() = runBlockingTest {
+        emptyValues()
+
+        viewModel.getUser("john123")
+
+        val response = viewModel.emptyValuesLiveData.getOrAwaitValueTest()
+
+        assert(response)
+    }
+
+    @Test
+    fun `should fetch user and return some user`() = runBlockingTest {
+        success()
+
+        viewModel.getUser("john123")
+
+        val response = viewModel.userLiveData.getOrAwaitValueTest()
+
+        assert(response.username.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user completed code challenge and return a error`() = runBlockingTest {
+        networkError()
+
+        viewModel.getCompletedCodeChallenge("john123")
+
+        val response = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(response.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user completed code challenge and return http error`() = runBlockingTest {
+        httpError()
+
+        viewModel.getCompletedCodeChallenge("john123")
+
+        val response = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(response.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user complete code challenge and return empty`() = runBlockingTest {
+        emptyValues()
+
+        viewModel.getCompletedCodeChallenge("john123")
+
+        val response = viewModel.emptyValuesLiveData.getOrAwaitValueTest()
+
+        assert(response)
+    }
+
+    @Test
+    fun `should fetch user complete code challenge and return some user`() = runBlockingTest {
+        success()
+
+        viewModel.getCompletedCodeChallenge("john123")
+
+        val response = viewModel.completedLiveData.getOrAwaitValueTest(10)
+
+        assert(response.completed.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user authored challenge and return a error`() = runBlockingTest {
+        networkError()
+
+        viewModel.getAuthoredChallenges("john123")
+
+        val response = viewModel.errorLiveData.getOrAwaitValueTest(0)
+
+        assert(response.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user authored challenge and return a http error`() = runBlockingTest {
+        httpError()
+
+        viewModel.getAuthoredChallenges("john123")
+
+        val response = viewModel.errorLiveData.getOrAwaitValueTest(5)
+
+        assert(response.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch user authored challenge and return empty`() = runBlockingTest {
+        emptyValues()
+
+        viewModel.getAuthoredChallenges("john123")
+
+        val response = viewModel.emptyValuesLiveData.getOrAwaitValueTest()
+
+        assert(response)
+    }
+
+    @Test
+    fun `should fetch user authored challenge and return some user`() = runBlockingTest  {
+        success()
+
+        viewModel.getAuthoredChallenges("john123")
+
+        val response = viewModel.authoredLiveData.getOrAwaitValueTest(10)
+
+        assert(response.challenges.isNotEmpty())
+    }
+
+    private fun networkError() {
+        fakeRepository.shouldReturnNetworkError = true
+        fakeRepository.shouldReturnEmptyValues = false
+        fakeRepository.shouldReturnHTTPError = false
+    }
+
+    private fun httpError() {
+        fakeRepository.shouldReturnNetworkError = false
+        fakeRepository.shouldReturnEmptyValues = false
+        fakeRepository.shouldReturnHTTPError = true
+    }
+
+    private fun emptyValues() {
+        fakeRepository.shouldReturnNetworkError = false
+        fakeRepository.shouldReturnEmptyValues = true
+        fakeRepository.shouldReturnHTTPError = false
+    }
+
+    private fun success() {
+        fakeRepository.shouldReturnNetworkError = false
+        fakeRepository.shouldReturnEmptyValues = false
+        fakeRepository.shouldReturnHTTPError = false
+    }
+}
